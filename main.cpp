482077d7bf13fcbdb598fb68b246275ee0fd0e61
null186 <null186@163.com>
2025-08-14 23:42:26 +0800

    modify main

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@
 int main() {
   auto thread = std::make_shared<mt::HandlerThread>("my-thread");
   thread->Start();
-//  auto handler = thread->handler();
-  auto handler = std::make_shared<mt::Handler>(thread->looper());
+  auto handler = thread->handler();
 
   handler->PostDelay([]() { std::cout << "Hello world, Delay!" << std::endl; },
                      std::chrono::seconds(5));
